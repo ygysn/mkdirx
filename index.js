0b61d33e852b8d2997ef16b786ace42fb6f2740d
ygysn <191929958+ygysn@users.noreply.github.com>
2025-01-04 12:26:35 +0800

    fix: fix bugs in the library

--- index.js
+++ index.js
@@ -14,8 +14,16 @@ function isRegularObject(v) {
 
 function joinFilePath(basePath, filePath, forceRelative) {
   return pathutils.isAbsolute(filePath) && !forceRelative
-  ? filePath
-  : pathutils.join(basePath, filePath);
+    ? filePath
+    : pathutils.join(basePath, filePath);
+}
+
+function resolveDestinationToTarget(basePath, dest, target) {
+  return joinFilePath(
+    basePath,
+    joinFilePath(pathutils.dirname(dest), target, true),
+    false,
+  );
 }
 
 const pathsList = {};
@@ -29,26 +37,41 @@ export function paths() {
 }
 
 /** Directive to create a new directive */
-function CustomDirective(func, props) {
+function CustomDirective(func, props = {}) {
+  if (typeof func !== 'function' || !isRegularObject(props)) {
+    throw new TypeError('Invalid argument passed to CustomDirective');
+  }
+
   this.$action = 'custom';
-  this.skipable = false;
-  this.exec = func.bind(this);
-  Object.assign(this, props);
+  this.function = func.bind({});
+  this.props = props;
+}
+
+CustomDirective.prototype.exec = function(basePath, filePath, options) {
+  return this.function(this.props, basePath, filePath, options);
 }
 
 
 /** Directive to create a new directive when creating a file or directory */
 function LazyDirective(func) {
+  if (typeof func !== 'function') {
+    throw new TypeError('Invalid argument passed to LazyDirective');
+  }
+
   this.$action = 'lazy';
   this.function = func;
 }
 
-LazyDirective.prototype.exec = async function(basePath, filePath) {
-  (await this.function(basePath, filePath)).exec();
+LazyDirective.prototype.exec = async function(basePath, filePath, options) {
+  await (await this.function(basePath, filePath, options)).exec(basePath, filePath, options);
 }
 
 /** Directive to move a file or directory to a path inside the base directory */
 function MoveDirective(target) {
+  if (typeof target !== 'string') {
+    throw new TypeError('Invalid argument passed to MoveDirective');
+  }
+
   this.$action = 'move';
   this.skipable = false;
   this.target = target;
@@ -56,65 +79,94 @@ function MoveDirective(target) {
 
 MoveDirective.prototype.exec = function(basePath, filePath) {
   const path = joinFilePath(basePath, filePath, true);
-  return fsp.rename(this.target, path);
+  const target = resolveDestinationToTarget(basePath, filePath, this.target);
+  return fsp.rename(target, path);
 }
 
 /** Directive to copy a file or directory to a path inside the base directory */
 function CopyDirective(target) {
+  if (typeof target !== 'string') {
+    throw new TypeError('Invalid argument passed to CopyDirective');
+  }
+
   this.$action = 'copy';
   this.skipable = false;
   this.target = target;
   this.dereferenceSymlink = false;
   this.filter = () => true;
-  this.recursive = false;
+  this.recursive = true;
   this.preserveTimestamps = false;
 }
 
 CopyDirective.prototype.$dereferenceSymlink = function(dereferenceSymlink) {
+  if (typeof dereferenceSymlink !== 'boolean') {
+    throw new TypeError('Invalid argument passed to MoveDirective.$dereferenceSymlink');
+  }
+
   this.dereferenceSymlink = dereferenceSymlink;
   return this;
 }
 
 CopyDirective.prototype.$filter = function(filter) {
+  if (typeof filter !== 'function') {
+    throw new TypeError('Invalid argument passed to MoveDirective.$filter');
+  }
+
   this.filter = filter;
   return this;
 }
 
 CopyDirective.prototype.$recursive = function(recursive) {
+  if (typeof recursive !== 'boolean') {
+    throw new TypeError('Invalid argument passed to MoveDirective.$recursive');
+  }
+
   this.recursive = recursive;
   return this;
 }
 
 CopyDirective.prototype.$preserveTimestamps = function(preserveTimestamps) {
+  if (typeof preserveTimestamps !== 'boolean') {
+    throw new TypeError('Invalid argument passed to MoveDirective.$preserveTimestamps');
+  }
+
   this.preserveTimestamps = preserveTimestamps;
 }
 
 CopyDirective.prototype.exec = function(basePath, filePath) {
   const path = joinFilePath(basePath, filePath, true);
-  return fsp.cp(this.target, path, {
+  const target = resolveDestinationToTarget(basePath, filePath, this.target);
+  return fsp.cp(target, path, {
     dereference: this.dereferenceSymlink,
     filter: this.filter,
-    force: this.force,
     recursive: this.recursive,
     preserveTimestamps: this.preserveTimestamps,
   });
 }
 
 /** Directive to create file, directory, symlink or hardlink inside the base directory */
-function CreateDirective(type, permisions, owner) {
+function CreateDirective(type, mode, owner) {
   this.$action = 'create';
   this.skipable = false;
   this.type = type;
-  this.permisions = permisions;
+  this.mode = mode;
   this.owner = owner;
 }
 
-CreateDirective.prototype.$permisions = function(permisions) {
-  this.permisions = permisions;
+CreateDirective.prototype.$mode = function(mode) {
+  if (typeof mode !== 'number') {
+    throw new TypeError(`Invalid argument passed to ${this.type[0].toUpperCase}${this.type.slice(1)}Directive.$mode`);
+  }
+
+  this.mode = mode;
   return this;
 }
 
 CreateDirective.prototype.$owner = function(uid, gid) {
+  if (typeof uid !== 'number' || typeof gid !== 'number') {
+    throw new TypeError(`Invalid argument passed to ${this.type[0].toUpperCase}${this.type.slice(1)}Directive.$owner`);
+  }
+
   this.owner = { uid, gid };
   return this;
 }
@@ -124,8 +176,8 @@ CreateDirective.prototype.applyFile = async function(file) {
     await file.chown(this.owner.uid, this.owner.gid);
   }
 
-  if (typeof this.permisions === 'number') {
-    await file.chmod(this.permisions);
+  if (typeof this.mode === 'number') {
+    await file.chmod(this.mode);
   }
 }
 
@@ -134,25 +186,33 @@ CreateDirective.prototype.applyPath = async function(path) {
     await fsp.chown(path, this.owner.uid, this.owner.gid);
   }
 
-  if (typeof this.permisions === 'number') {
-    await fsp.chmod(path, this.permisions);
+  if (typeof this.mode === 'number') {
+    await fsp.chmod(path, this.mode);
   }
 }
 
 /** Directive to create file inside the base directory */
 function FileDirective() {
-  CreateDirective.call(this, 'file', 0o644, undefined);
-  this.skipable = true;
+  CreateDirective.call(this, 'file', undefined, undefined);
+  this.skipable = false;
   this.content = Buffer.alloc(0);
 }
 
 Object.setPrototypeOf(FileDirective.prototype, CreateDirective.prototype);
 FileDirective.prototype.$write = function(data) {
+  if (typeof data !== 'string' && !(data instanceof Buffer)) {
+    throw new TypeError(`Invalid argument passed to FileDirective.$write`);
+  }
+
   this.content = Buffer.from(data);
   return this;
 }
 
 FileDirective.prototype.$append = function(data) {
+  if (typeof data !== 'string' && !(data instanceof Buffer)) {
+    throw new TypeError(`Invalid argument passed to FileDirective.$append`);
+  }
+
   const buff = Buffer.from(data);
   this.content = Buffer.concat([this.content, buff], this.content.length + buff.length);
   return this;
@@ -160,7 +220,6 @@ FileDirective.prototype.$append = function(data) {
 
 FileDirective.prototype.exec = async function(basePath, filePath) {
   const path = joinFilePath(basePath, filePath, true);
-  await mkdirWhenNotExists(pathutils.dirname(path));
   const file = await fsp.open(path, 'w')
   await file.write(this.content);
   await this.applyFile(file);
@@ -174,14 +233,17 @@ function DirectoryDirective(files) {
   this.files = files ?? {};
 }
 
-DirectoryDirective.prototype.$files = function(files) {
+DirectoryDirective.prototype.$expand = function(files) {
+  if (!isRegularObject(files)) {
+    throw new TypeError(`Invalid argument passed to DirectoryDirective.$expand`);
+  }
+
   this.files = Object.assign({}, this.files, files);
   return this;
 }
 
 DirectoryDirective.prototype.exec = async function(basePath, dirPath, options) {
   const path = joinFilePath(basePath, dirPath, true);
-  await mkdirWhenNotExists(path);
   await mkdirx(path, this.files, Object.assign(options, {
     force: true,
   }));
@@ -197,43 +259,35 @@ function SymlinkDirective(target, symlinkType = 'file') {
 }
 
 SymlinkDirective.prototype.$symlinkType = function(symlinkType) {
-  this.symlinkType = symlinkType;
-  return this;
-}
+  if (!['dir', 'file', 'junction', null].includes(symlinkType)) {
+    throw new TypeError(`Invalid argument passed to FileDirective.$symlinkType`);
+  }
 
-SymlinkDirective.prototype.$target = function(target) {
-  this.target = target;
+  this.symlinkType = symlinkType ?? 'file';
   return this;
 }
 
 SymlinkDirective.prototype.exec = async function(basePath, filePath) {
   const path = joinFilePath(basePath, filePath, true);
-  await mkdirWhenNotExists(pathutils.dirname(path));
   await fsp.symlink(this.target, path, this.symlinkType);
 }
 
 /** Directive to create hardlink inside the base directory */
 function HardLinkDirective(target) {
   CreateDirective.call(this, 'hardlink', undefined, undefined);
-  this.skipable = true;
+  this.skipable = false;
   this.target = target;
-}
-
-HardLinkDirective.prototype.$target = function(target) {
-  this.target = target;
-  return this;
 }
 
 HardLinkDirective.prototype.exec = async function(basePath, filePath) {
   const path = joinFilePath(basePath, filePath, false);
-  const target = joinFilePath(basePath, this.target, false);
-  await mkdirWhenNotExists(pathutils.dirname(path));
+  const target = resolveDestinationToTarget(basePath, filePath, this.target);
   await fsp.link(target, path);
 }
 
 /** Helper function to create CustomDirective */
-export function custom(func) {
-  return new CustomDirective(func);
+export function custom(func, props) {
+  return new CustomDirective(func, props);
 }
 
 /** Helper function to create LazyDirective */
@@ -274,7 +328,7 @@ export function link(target) {
 /** Create directories and files based on object mappings between filenames and directives */
 export async function mkdirx(basePath, files, options = {}) {
   if (typeof basePath !== 'string' || !basePath || !isRegularObject(files) || !isRegularObject(options)) {
-    throw new Error('Invalid arguments given');
+    throw new Error('Invalid arguments passed to mkdirx');
   } else if (fs.existsSync(basePath) && !options.force) {
     throw new Error(`${basePath} directory already exists`);
   }
@@ -283,12 +337,12 @@ export async function mkdirx(basePath, files, options = {}) {
   for (const pathsSymbol of Object.getOwnPropertySymbols(files)) {
     const fileInfo = files[pathsSymbol];
     if (!pathsList[pathsSymbol]) {
-      throw new Error('');
+      throw new TypeError(`Invalid path, '${pathsSymbol.toString()}' is unknown`);
     }
 
     for (const fileName of pathsList[pathsSymbol]) {
       if (tmpFiles[fileName]) {
-        throw new Error('');
+        throw new Error(`${fileName} is duplicate`);
       }
       tmpFiles[fileName] = fileInfo;
     }
@@ -301,18 +355,23 @@ export async function mkdirx(basePath, files, options = {}) {
   for (const filePath of Object.keys(tmpFiles)) {
     const fileDirective = tmpFiles[filePath];
     if (typeof fileDirective.$type !== 'string' && typeof fileDirective.exec !== 'function') {
-      throw new Error('');
+      throw new TypeError('Invalid object files');
     }
 
+    await mkdirWhenNotExists(pathutils.dirname(pathutils.join(basePath, filePath)));
     if (fileDirective.skipable) {
       executePromises.push(fileDirective.exec(basePath, filePath, options));
     } else {
       await fileDirective.exec(basePath, filePath, options);
     }
   }
-  await Promise.all(executePromises);
+
+  if (executePromises.length !== 0) {
+    await Promise.all(executePromises);
+  }
 }
 
+mkdirx.paths = paths;
 mkdirx.custom = custom;
 mkdirx.lazy = lazy;
 mkdirx.move = move;
